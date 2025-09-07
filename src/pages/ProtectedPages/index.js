0fff24dc0c1cb3eb582a14acc5a9f42d93e4ed6d
Steven Edgar Phiri <stevenphiri151@gmail.com>
2025-09-07 15:43:59 +0200

    Refactor project structure and update dependencies
    Removed unused components, pages, and utility files, and restructured authentication and dashboard features. Migrated from Prettier to ESLint for code linting, updated .gitignore, and replaced favicon and logo assets. Updated dependencies in package.json and package-lock.json to latest versions, and improved jsconfig.json configuration for better path resolution.

--- src/pages/ProtectedPages/index.js
+++
@@ -1,4 +0,0 @@
-export * from './admin-dashboard';
-export * from './company-dashboard';
-export * from './user-dashboard';
-export * from './lists';
--- src/pages/ProtectedPages/lists/webhook/page.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Webhook = () => {
-  return <div>Webhook</div>;
-};
-
-export default Webhook;
