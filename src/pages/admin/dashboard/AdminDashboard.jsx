040a2886b24234fc246b3ac10daa9a4f8dd459f4
Steven Edgar Phiri <stevenphiri151@gmail.com>
2025-12-05 10:17:24 +0200

    fix 2

--- src/pages/admin/dashboard/AdminDashboard.jsx
+++ src/pages/admin/dashboard/AdminDashboard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Header } from "@/components/common";
-import SideBar from "../../../components/admin/dashboard/SideBar";
+import SideBar from "@/components/admin/dashboard/SideBar";
 import AdminDashboardOverview from "@/components/admin/dashboard/AdminDashboardOverview";
 import StaffSchedule from "@/components/admin/dashboard/StaffSchedule";
 import StockUpdate from "@/components/admin/dashboard/StockUpdate";
@@ -11,13 +11,13 @@ const AdminDashboard = () => {
     <div className="flex h-screen ">
       <SideBar />
 
-      <div className="flex overflow-hidden flex-col flex-1">
+      <div className="flex flex-col flex-1 overflow-hidden">
         <Header />
 
-        <div className="overflow-y-auto flex-1">
+        <div className="flex-1 overflow-y-auto">
           <AdminDashboardOverview />
 
-          <div className="flex gap-3 mb-3 sm:gap-4 px-4">
+          <div className="flex gap-3 px-4 mb-3 sm:gap-4">
             <TotalStaff />
             <StockUpdate />
           </div>
