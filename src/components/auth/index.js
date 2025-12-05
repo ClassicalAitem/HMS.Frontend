8d933ac79242005556ce0636802d82a61209ce8b
Steven Edgar Phiri <stevenphiri151@gmail.com>
2025-12-05 09:57:03 +0200

    This is a fix

--- src/components/auth/index.js
+++
@@ -1,5 +0,0 @@
-// Auth components barrel exports
-export { default as AuthLayout } from './AuthLayout';
-export { default as AuthLogo } from './AuthLogo';
-export { default as AuthInput } from './AuthInput';
-export { default as CarouselComponent } from './CarouselComponent';
--- src/components/frontdesk/index.js
+++
@@ -1,5 +0,0 @@
-// Frontdesk dashboard components
-export { default as Sidebar } from './dashboard/Sidebar';
-export { default as UpcomingSurgeries } from './dashboard/UpcomingSurgeries';
-export { default as OverallDischarge } from './dashboard/OverallDischarge';
-export { default as RecentlyAddedPatients } from './dashboard/RecentlyAddedPatients';
